effffe5d4f209ae186bd2a11529c457e60bdb540
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-08-07 19:39:30 -0400

    ex1

--- src/com/Manbir/chapterbigO/Example1.java
+++ src/com/Manbir/chapterbigO/Example1.java
@@ -1,4 +1,20 @@
 package com.Manbir.chapterbigO;
 
 public class Example1 {
+    public static void main(String[] args) {
+
+
+        System.out.println(sum(4));
+
+
+    }
+
+    public static int sum(int n) {
+        if (n < 0) {
+            return 0;
+        } else {
+            return n + sum(n-1);
+        }
+    }
+
 }
